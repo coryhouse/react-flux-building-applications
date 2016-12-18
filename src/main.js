d3e4ac1220d899cecd7af6e8965fc4719299e1cc
Cory House <housecor@gmail.com>
2016-12-18 09:43:10 -0600

    Add react-dom and update router call

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@
 
 var React = require('react');
 var ReactDom = require('react-dom');
-var Router = require('react-router');
 var routes = require('./routes');
 var InitializeActions = require('./actions/initializeActions');
 
 InitializeActions.initApp();
 
-Router.run(routes, function(Handler) {
-	ReactDom.render(<Handler/>, document.getElementById('app'));
-});
+ReactDom.render(routes, document.getElementById('app'));
