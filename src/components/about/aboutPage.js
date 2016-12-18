bd372ea06041b9f3fcadcdff7db2fea43400ab24
Cory House <housecor@gmail.com>
2016-12-18 09:45:47 -0600

    Transitions now handled in routes.js in react-router 3.0

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,21 +3,6 @@
 var React = require('react');
 
 var About = React.createClass({
-	statics: {
-		willTransitionTo: function(transition, params, query, callback) {
-			if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
-				transition.about();
-			} else {
-				callback();
-			}
-		},
-		
-		willTransitionFrom: function(transition, component) {
-			if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
-				transition.about();
-			}
-		}
-	},
 	render: function () {
 		return (
 			<div>
