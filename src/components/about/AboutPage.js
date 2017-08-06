018a84c2916f475d8cc02fdc6e44ff76fdf29875
Cory House <housecor@gmail.com>
2017-08-06 16:40:10 -0500

    markup fix

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -4,17 +4,15 @@ function About() {
 	return (
 		<div>
 			<h1>About</h1>
-			<p>
-				This application uses the following technologies:
-					<ul>
-					<li>React</li>
-					<li>React Router</li>
-					<li>Flux</li>
-					<li>Node</li>
-					<li>npm Scripts</li>
-					<li>Webpack</li>
-				</ul>
-			</p>
+			This application uses the following technologies:
+				<ul>
+				<li>React</li>
+				<li>React Router</li>
+				<li>Flux</li>
+				<li>Node</li>
+				<li>npm Scripts</li>
+				<li>Webpack</li>
+			</ul>
 		</div>
 	);
 };
