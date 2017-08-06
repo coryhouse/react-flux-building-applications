a802ec45b3f964982024cf4a817f10314ecd093a
Cory House <housecor@gmail.com>
2017-08-06 10:13:06 -0500

    Comment out unused

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import { render } from 'react-dom';
 import App from './components/App';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-injectTapEventPlugin();
+// injectTapEventPlugin();
 render(
   <Router>
-    <MuiThemeProvider>
-      <Route path="/" component={App} />
-    </MuiThemeProvider>
+    <Route path="/" component={App} />
   </Router>, document.getElementById('root'));
 registerServiceWorker();
