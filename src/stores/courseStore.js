6e1e8e5d2dc391926b634ca27f6338a9482b9f94
Cory House <housecor@gmail.com>
2017-08-06 13:12:09 -0500

    fix store

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,58 +1,58 @@
 import Dispatcher from '../dispatcher/appDispatcher';
 import ActionTypes from '../constants/actionTypes';
-import {EventEmitter} from 'events';
+import { EventEmitter } from 'events';
 import _ from 'lodash';
 const CHANGE_EVENT = 'change';
 
-const _courses = [];
+let _courses = [];
 
-const CourseStore = Object.assign({}, EventEmitter.prototype, {
-	addChangeListener: function(callback) {
+const courseStore = Object.assign({}, EventEmitter.prototype, {
+	addChangeListener: function (callback) {
 		this.on(CHANGE_EVENT, callback);
 	},
 
-	removeChangeListener: function(callback) {
+	removeChangeListener: function (callback) {
 		this.removeListener(CHANGE_EVENT, callback);
 	},
 
-	emitChange: function() {
+	emitChange: function () {
 		this.emit(CHANGE_EVENT);
 	},
 
-	getAllCourses: function() {
+	getAllCourses: function () {
 		return _courses;
 	},
 
-	getCourseById: function(id) {
-		return _.find(_courses, {id: id});
+	getCourseById: function (id) {
+		return _.find(_courses, { id: id });
 	}
 });
 
-Dispatcher.register(function(action) {
-	switch(action.actionType) {
+Dispatcher.register(function (action) {
+	switch (action.actionType) {
 		case ActionTypes.LOAD_COURSES:
 			_courses = action.courses;
-			CourseStore.emitChange();
+			courseStore.emitChange();
 			break;
 		case ActionTypes.CREATE_COURSE:
 			_courses.push(action.course);
-			CourseStore.emitChange();
+			courseStore.emitChange();
 			break;
 		case ActionTypes.UPDATE_COURSE:
-			const existingCourse = _.find(_courses, {id: action.course.id});
-			const existingCourseIndex = _.indexOf(_courses, existingCourse); 
+			const existingCourse = _.find(_courses, { id: action.course.id });
+			const existingCourseIndex = _.indexOf(_courses, existingCourse);
 			_courses.splice(existingCourseIndex, 1, action.course);
-			CourseStore.emitChange();
-			break;	
+			courseStore.emitChange();
+			break;
 		case ActionTypes.DELETE_COURSE:
-			_.remove(_courses, function(course) {
+			_.remove(_courses, function (course) {
 				return action.id === course.id;
 			});
-			CourseStore.emitChange();
+			courseStore.emitChange();
 			break;
 		default:
-			// no op
+		// no op
 	}
 });
 
-export default CourseStore;
+export default courseStore;
