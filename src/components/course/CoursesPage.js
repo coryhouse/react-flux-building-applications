11cc441a67c084eb73e4f196c03d0f219fe64f6b
Cory House <housecor@gmail.com>
2017-08-06 11:00:03 -0500

    Fix add course link

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CourseList from './CourseList';
 import {getAllCourses} from '../../api/courseApi';
-// import {browserHistory} from 'react-router';
+import {Link} from 'react-router-dom';
 
 class CoursesPage extends React.Component {
   constructor(props) {
@@ -25,10 +25,7 @@ class CoursesPage extends React.Component {
     return (
       <div>
         <h1>Courses</h1>
-        <input type="submit"
-               value="Add Course"
-               className="btn btn-primary"
-               onClick={this.redirectToAddCoursePage}/>
+        <Link to="/course">Add Course</Link>
         <CourseList courses={this.state.courses}/>
       </div>
     );
