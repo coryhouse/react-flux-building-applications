d843760f6f14c86764a016a15405821853280d05
Cory House <housecor@gmail.com>
2017-08-09 21:38:10 -0500

    Load courses on app load

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 import App from './components/App';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import {loadCourses} from './actions/courseActions';
+
+loadCourses();
 
 render(
   <Router>
