7fcf275b5294fb12a5cdc421505f1263ef4b1712
Cory House <housecor@gmail.com>
2017-08-09 21:28:18 -0500

    Remove material-ui stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-// injectTapEventPlugin();
 render(
   <Router>
     <Route path="/" component={App} />
