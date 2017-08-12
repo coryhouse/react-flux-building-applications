1305c2d12488272d8752a55b1276c41bf8692d24
Cory House <housecor@gmail.com>
2017-08-12 07:58:54 -0500

    Add imports with comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 import App from './components/App';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+// Note: I don't technically need these imports since they're imported on ManageCoursePage.js, but 
+// it's nice to import them here for clarity.
+//import authorStore from './stores/authorStore'; // eslint-disable-line no-unused-vars
+//import courseStore from './stores/courseStore'; // eslint-disable-line no-unused-vars
 import {loadCourses} from './actions/courseActions';
 import {loadAuthors} from './actions/authorActions';
 
