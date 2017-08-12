076c6739ff70518d83afdd6f703977a290671ef6
Cory House <housecor@gmail.com>
2017-08-12 08:38:35 -0500

    Add comment

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { render } from 'react-dom';
 import App from './components/App';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-// Note: I don't technically need these imports since they're imported on ManageCoursePage.js, but 
-// it's nice to import them here for clarity.
-//import authorStore from './stores/authorStore'; // eslint-disable-line no-unused-vars
-//import courseStore from './stores/courseStore'; // eslint-disable-line no-unused-vars
+// Note: I don't technically need these imports since they're imported 
+// on ManageCoursePage.js, but for the calls to loadAuthors and loadCourses
+// to work below, these must be imported first so that they're registered
+// with the dispatcher, so doing so here. (it coincidentally works without
+// these, but leaving these in to be clear and deterministic)
+import authorStore from './stores/authorStore'; // eslint-disable-line no-unused-vars
+import courseStore from './stores/courseStore'; // eslint-disable-line no-unused-vars
 import {loadCourses} from './actions/courseActions';
 import {loadAuthors} from './actions/authorActions';
 
