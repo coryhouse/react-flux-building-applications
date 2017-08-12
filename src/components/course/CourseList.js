2913c37ed0e8759384aedda567725d18b48f2850
Cory House <housecor@gmail.com>
2017-08-12 11:45:15 -0500

    Add delete course support

--- src/components/course/CourseList.js
+++ src/components/course/CourseList.js
@@ -1,37 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const CourseList = ({courses}) => {
+function CourseList({ courses, deleteCourse }) {
   return (
     <table className="table">
       <thead>
-      <tr>
-        <th>&nbsp;</th>
-        <th>Title</th>
-        <th>Author</th>
-        <th>Category</th>
-        <th>Length</th>
-      </tr>
+        <tr>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+          <th>Length</th>
+        </tr>
       </thead>
       <tbody>
-      {
-        courses.map(course =>
-        <tr key={course.id}>
-          <td><a href={course.watchHref}>Watch</a></td>
-          <td><Link to={'/course/' + course.id}>{course.title}</Link></td>
-          <td>{course.authorId}</td>
-          <td>{course.category}</td>
-          <td>{course.length}</td>
-        </tr>
-      )}
+        {
+          courses.map(course => {
+            return (
+              <tr key={course.id}>
+                <td><button onClick={deleteCourse} value={course.id}>Delete</button></td>
+                <td><a href={course.watchHref}>Watch</a></td>
+                <td><Link to={'/course/' + course.id}>{course.title}</Link></td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+                <td>{course.length}</td>
+              </tr>
+            )
+          })
+        }
       </tbody>
     </table>
   );
 };
 
 CourseList.propTypes = {
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  deleteCourse: PropTypes.func.isRequired
 };
 
 export default CourseList;
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import CourseList from './CourseList';
 import courseStore from '../../stores/courseStore';
-import {Link} from 'react-router-dom';
+import { deleteCourse } from '../../actions/courseActions';
+import { Link } from 'react-router-dom';
 
 class CoursesPage extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        courses: courseStore.getAllCourses()
-      }
+    super(props);
+    this.state = {
+      courses: courseStore.getAllCourses()
+    }
   }
- 
- 	componentWillMount() {
- 		courseStore.addChangeListener(this.onChange);
- 	}
- 
- 	componentWillUnmount() {
- 		courseStore.removeChangeListener(this.onChange);
-   }
-  
+
+  componentWillMount() {
+    courseStore.addChangeListener(this.onChange);
+  }
+
+  componentWillUnmount() {
+    courseStore.removeChangeListener(this.onChange);
+  }
+
   onChange = () => {
- 		this.setState({ courses: courseStore.getAllCourses() });
- 	}
+    this.setState({ courses: courseStore.getAllCourses() });
+  }
+
+  onDeleteCourseClick = (event) => {
+    deleteCourse(event.target.value);
+  }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
         <Link to="/course">Add Course</Link>
-        <CourseList courses={this.state.courses}/>
+        <CourseList courses={this.state.courses} deleteCourse={this.onDeleteCourseClick} />
       </div>
     );
   }
