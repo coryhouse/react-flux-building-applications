08cc653129dfd92ffb4bdd19d4370594edbb77ab
Cory House <Cory.House@coxautoinc.com>
2017-12-26 13:59:44 -0600

    Move routes to app.js since React Router 4 supports dynamic routes.

--- src/components/app.js
+++ src/components/app.js
@@ -3,14 +3,37 @@
 var React = require('react');
 var Header = require('./common/header');
 $ = jQuery = require('jquery');
+var createReactClass = require('create-react-class');
+var ReactRouter = require('react-router-dom');
+var HomePage = require('./homePage');
+var AuthorPage = require('./authors/authorPage');
+var ManageAuthorPage = require('./authors/manageAuthorPage');
+var AboutPage = require('./about/aboutPage');
+var NotFoundPage = require('./notFoundPage');
+var Header = require('./common/header');
 
-var App = React.createClass({
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Redirect = ReactRouter.Redirect;
+
+var App = createReactClass({
 	render: function() {
 		return (
 			<div>
 				<Header/>
 				<div className="container-fluid">
-					{this.props.children}
+					<Switch>
+						{ /* Redirect typos */ }
+						<Redirect from="/awthurs" to="/authors"/>
+						<Redirect from="/about-us" to="/about"/>
+
+						<Route exact path="/" component={HomePage}/>
+						<Route path="/authors" component={AuthorPage}/>
+						<Route path="/author" component={ManageAuthorPage} exact />
+						<Route path="/author/:id" component={ManageAuthorPage}/>
+						<Route path="/about" component={AboutPage}/>
+						<Route component={NotFoundPage}/>
+					</Switch>
 				</div>
 			</div>
 		);
