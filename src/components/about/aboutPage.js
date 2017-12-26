e375cbcfab91a9b519d592cc31a52b260879f2df
Cory House <Cory.House@coxautoinc.com>
2017-12-26 08:58:49 -0600

    Switch to create-react-class and React Router 4's Prompt

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -1,22 +1,15 @@
 "use strict";
 
 var React = require('react');
+var createReactClass = require('create-react-class');
+var Prompt = require('react-router-dom').Prompt;
 var withRouter = require('react-router').withRouter;
 
-var About = React.createClass({
-	componentDidMount: function() {
-		this.props.router.setRouteLeaveHook(this.props.route, this.routerWillLeave)
-	},
-
-	routerWillLeave: function(nextLocation) {
-		// return false to prevent a transition w/o prompting the user,
-		// or return a string to allow the user to decide:
-		return 'Are you sure you want to leave a page that\'s this exciting?';
-	},
-
+var About = createReactClass({
 	render: function () {
 		return (
 			<div>
+				<Prompt message="Are you sure you want to leave a page this exiting?" />
 				<h1>About</h1>
 				<div>
 					This application uses the following technologies:
