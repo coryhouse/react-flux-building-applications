97a38079d51ffb725d02665778e203f18bce3236
Cory House <housecor@gmail.com>
2019-03-23 13:14:16 -0500

    Disable api delay

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -30,7 +30,7 @@ server.use(jsonServer.bodyParser);
 
 // Simulate delay on all requests
 server.use(function(req, res, next) {
-  setTimeout(next, 2000);
+  setTimeout(next, 0);
 });
 
 // Declaring custom routes below. Add custom routes before JSON Server router
