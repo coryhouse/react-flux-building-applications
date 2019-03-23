229024e83c38fb3e572230804287924b3cff0c8d
Cory House <housecor@gmail.com>
2019-03-23 11:40:32 -0500

    Reword

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="jumbotron">
       <h1>Pluralsight Administration</h1>
-      <p>React, React Router, and Flux for ultra-responsive web apps.</p>
+      <p>React, Flux, and React Router for ultra-responsive web apps.</p>
       <Link to="about" className="btn btn-primary btn-lg">
         Learn more
       </Link>
