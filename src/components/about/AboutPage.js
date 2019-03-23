d5630c656baaabb2f7bedcb94397e9510100e9ee
Cory House <housecor@gmail.com>
2019-03-23 11:26:06 -0500

    Simplify

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -4,15 +4,7 @@ function About() {
   return (
     <div>
       <h1>About</h1>
-      This application uses the following technologies:
-      <ul>
-        <li>React</li>
-        <li>React Router</li>
-        <li>Flux</li>
-        <li>Node</li>
-        <li>npm Scripts</li>
-        <li>Webpack</li>
-      </ul>
+      This application uses React.
     </div>
   );
 }
