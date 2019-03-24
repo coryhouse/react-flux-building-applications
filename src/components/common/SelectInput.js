a2921447a4958e4996cf47114dce3493e1c83ba4
Cory House <housecor@gmail.com>
2019-03-24 08:59:02 -0500

    Accept number or type in select

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -39,7 +39,7 @@ SelectInput.propTypes = {
   label: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   defaultOption: PropTypes.string,
-  value: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   error: PropTypes.string,
   options: PropTypes.arrayOf(PropTypes.object).isRequired
 };
