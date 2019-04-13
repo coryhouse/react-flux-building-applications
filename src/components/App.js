8a7d0b578ae0c84c6d224b39b14f968197a4a0a2
Cory House <housecor@gmail.com>
2019-04-13 13:55:57 -0400

    Switch to func

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./common/Header";
 import HomePage from "./HomePage";
@@ -7,23 +7,21 @@ import ManageCoursePage from "./course/ManageCoursePage.Hooks"; //eslint-disable
 import AboutPage from "./about/AboutPage";
 import NotFoundPage from "./NotFoundPage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/courses" component={CoursesPage} />
-          <Route path="/course/:id" component={ManageCoursePage} />
-          <Route path="/course" component={ManageCoursePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/404" component={NotFoundPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:id" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/404" component={NotFoundPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
