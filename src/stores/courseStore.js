779637a1ef588d67afab9f665e8509e31fde7323
Cory House <housecor@gmail.com>
2019-04-27 14:58:40 -0500

    Use class and extends FTW

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,41 +1,43 @@
-import Dispatcher from "../dispatcher/appDispatcher";
-import actionTypes from "../constants/actionTypes";
+import Dispatcher from "../appDispatcher";
+import actionTypes from "../actions/actionTypes";
 import { EventEmitter } from "events";
 const CHANGE_EVENT = "change";
 
 let _courses = [];
 
-const courseStore = Object.assign({}, EventEmitter.prototype, {
-  addChangeListener: function(callback) {
+class courseStore extends EventEmitter {
+  addChangeListener(callback) {
     this.on(CHANGE_EVENT, callback);
-  },
+  }
 
-  removeChangeListener: function(callback) {
+  removeChangeListener(callback) {
     this.removeListener(CHANGE_EVENT, callback);
-  },
+  }
 
-  emitChange: function() {
+  emitChange() {
     this.emit(CHANGE_EVENT);
-  },
+  }
 
-  getCourses: function() {
+  getCourses() {
     return _courses;
-  },
+  }
 
-  getCourseById: function(id) {
+  getCourseById(id) {
     return _courses.find(c => c.id === parseInt(id, 10));
   }
-});
+}
+
+const store = new courseStore();
 
 Dispatcher.register(function(action) {
   switch (action.actionType) {
     case actionTypes.LOAD_COURSES:
       _courses = action.courses;
-      courseStore.emitChange();
+      store.emitChange();
       break;
     case actionTypes.CREATE_COURSE:
       _courses.push(action.course);
-      courseStore.emitChange();
+      store.emitChange();
       break;
     case actionTypes.UPDATE_COURSE:
       // Update the relevant course
@@ -45,15 +47,15 @@ Dispatcher.register(function(action) {
         }
         return course;
       });
-      courseStore.emitChange();
+      store.emitChange();
       break;
     case actionTypes.DELETE_COURSE:
       _courses = _courses.filter(c => c.id !== parseInt(action.id, 10));
-      courseStore.emitChange();
+      store.emitChange();
       break;
     default:
     // no op
   }
 });
 
-export default courseStore;
+export default store;
