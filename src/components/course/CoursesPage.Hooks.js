7b744d3797a4f2a4c0ef125a1b0dfcc49fb97779
Cory House <housecor@gmail.com>
2019-04-28 11:15:55 -0500

    Add hooks version

---
+++ src/components/course/CoursesPage.Hooks.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import courseStore from "../../stores/courseStore";
+import { deleteCourse, loadCourses } from "../../actions/courseActions";
+import CourseList from "./CourseList";
+import { Link } from "react-router-dom";
+
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    courseStore.addChangeListener(onChange);
+    if (courseStore.getCourses().length === 0) loadCourses();
+    return () => courseStore.removeChangeListener(onChange); // cleanup on unmount
+  }, []);
+
+  function onChange() {
+    setCourses(courseStore.getCourses());
+  }
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <Link className="btn btn-primary add-course" to="/course">
+        Add Course
+      </Link>
+      <CourseList courses={courses} deleteCourse={deleteCourse} />
+    </>
+  );
+}
+
+export default CoursesPage;
