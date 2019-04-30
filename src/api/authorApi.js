1f58398babb87550bd7cf6cb5aee588247c022ca
Cory House <housecor@gmail.com>
2019-04-30 07:07:40 -0500

    Add apis for save and delete author

--- src/api/authorApi.js
+++ src/api/authorApi.js
@@ -6,3 +6,19 @@ export function getAuthors() {
     .then(handleResponse)
     .catch(handleError);
 }
+
+export function saveAuthor(author) {
+  return fetch(baseUrl + (author.id || ""), {
+    method: author.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(author)
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function deleteAuthor(authorId) {
+  return fetch(baseUrl + authorId, { method: "DELETE" })
+    .then(handleResponse)
+    .catch(handleError);
+}
